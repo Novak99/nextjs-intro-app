3c40b7c75cc7c7911fabb52f499168613ae5aac7
Novak Lukic <novak.lukic@adera.no>
2024-06-28 20:15:35 +0200

    Fetch through server action

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -2,25 +2,25 @@ import ToDoList from '@/components/ToDoList'
 import db from '@/utils/db'
 import { redirect } from 'next/navigation'
 
-// const getData = async () => {
-//     await new Promise<void>((resolve, reject) => {
-//         setTimeout(() => resolve(), 500)
-//     })
-//     const todos = await db.todo.findMany({})
-    
-//     return todos
-// }
-
 const getData = async () => {
-    const response = await fetch('http://localhost:3000/api/todo', {
-        credentials: 'include'
+    await new Promise<void>((resolve, reject) => {
+        setTimeout(() => resolve(), 500)
     })
-    const data = await response.json()
-    if (!data.todos) {
-        return
-    }
-    return data.todos
+    const todos = await db.todo.findMany({})
+    
+    return todos
 }
+
+// const getData = async () => {
+//     const response = await fetch('http://localhost:3000/api/todo', {
+//         credentials: 'include'
+//     })
+//     const data = await response.json()
+//     if (!data.todos) {
+//         return
+//     }
+//     return data.todos
+// }
 
 const ToDoPage = async () => {
 
